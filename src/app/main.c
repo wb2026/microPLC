33562849e2b889c813e97a3a4a0bcf7bdfafb016
wb2026 <wb20251231@163.com>
2026-04-03 14:20:43 +0800

    can enter main
    main is ok

---
+++ src/app/main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+
+
+
+int main(void) {
+	
+	
+	
+	
+	
+	
+	
+	for(;;) {
+	}
+}
---
+++ src/app/main.h
@@ -0,0 +1,15 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include "n32g43x.h" 
+
+
+
+
+
+
+
+
+
+
+#endif
